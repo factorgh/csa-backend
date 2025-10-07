2c6128ba1d01cf7483745bab479c6acfc4ec87d2
factorgh <burchellsbale@gmail.com>
2025-10-07 08:50:27 +0000

    update on sever timeouts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,27 @@
-import http from 'http';
-import mongoose from 'mongoose';
-import app from './app';
-import config from './config';
+import http from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
 
 const server = http.createServer(app);
+
+// Request must fully arrive within 2 minutes
+server.requestTimeout = 2 * 60 * 1000; // 120_000 ms
+
+// Headers must arrive within 30 seconds
+server.headersTimeout = 30 * 1000; // 30_000 ms
+
+// Idle socket timeout (no data activity)
+server.timeout = 2 * 60 * 1000; // 120_000 ms
+
+// Keep-alive idle timeout for persistent connections
+server.keepAliveTimeout = 30 * 1000; // 10_000 ms
 
 async function start() {
   try {
     await mongoose.connect(config.mongoUri);
     // eslint-disable-next-line no-console
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
 
     server.listen(config.port, () => {
       // eslint-disable-next-line no-console
@@ -17,20 +29,20 @@ async function start() {
     });
   } catch (err) {
     // eslint-disable-next-line no-console
-    console.error('Failed to start server:', err);
+    console.error("Failed to start server:", err);
     process.exit(1);
   }
 }
 
 start();
 
-process.on('unhandledRejection', (reason) => {
+process.on("unhandledRejection", (reason) => {
   // eslint-disable-next-line no-console
-  console.error('Unhandled Rejection:', reason);
+  console.error("Unhandled Rejection:", reason);
 });
 
-process.on('uncaughtException', (err) => {
+process.on("uncaughtException", (err) => {
   // eslint-disable-next-line no-console
-  console.error('Uncaught Exception:', err);
+  console.error("Uncaught Exception:", err);
   process.exit(1);
 });
