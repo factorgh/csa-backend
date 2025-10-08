45e907bf6863a4975d71677d67cd8238f372e005
factorgh <burchellsbale@gmail.com>
2025-10-08 17:12:31 +0000

    update on cors

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const app = express();
 
 // Security & Middlewares
 app.use(helmet());
-app.use(cors({ origin: config.cors.origin, credentials: true }));
+app.use(cors({ origin: "*" }));
 app.use(mongoSanitize());
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
